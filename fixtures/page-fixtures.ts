70c93c65d0513f24611b9cc6ae25551c0ed5252a
LNU <2438030@cognizant.com>
2025-10-07 18:56:04 +0530

    updated

--- fixtures/page-fixtures.ts
+++ fixtures/page-fixtures.ts
@@ -1,6 +1,7 @@
-import { Page } from '@playwright/test';
+// import { Page } from '@playwright/test';
 import { CarInsurance } from '../pages/CarInsurance';
 import { CarDummy } from '../pages/CarDummy'; // ✅ Correct import path
+import { test as baseTest, expect, Page } from '@playwright/test';
 
 export class PageFixture {
   carInsurance: CarInsurance;
@@ -11,3 +12,15 @@ export class PageFixture {
     this.carDummy = new CarDummy(page); // ✅ Proper constructor usage
   }
 }
+type Fixtures = {
+  pages: PageFixture;
+};
+ 
+export const test = baseTest.extend<Fixtures>({
+  pages: async ({ page }, use) => {
+    await use(new PageFixture(page));
+  },
+});
+ 
+export { expect };
+
